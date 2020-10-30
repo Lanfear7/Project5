b65f3c32e77656dc3c18b62e0a1db8171c0ed143
Lanfear7 <brennan1086@gmail.com>
2020-10-30 00:15:46 -0700

    clicking on each user will bring up a display of user info

--- js/script.js
+++ js/script.js
@@ -2,17 +2,18 @@
 fetchData = (url) => {
     return fetch(url)
         .then(data => data.json())
-        .then(data => jsonData = data.results)
-        .then(data => createHTML(data)) //will return the json of the data we have requested from the url provided
+        .then(data => jsonData = data.results) //will return the json of the data we have requested from the url provided
+        .then(data => createHTML(data)) //will creat new card elements that go to the DOM
+        .then(data => addEvent(data)) //will add the event listener to each card
 }
 fetchData('https://randomuser.me/api/?results=12')
 
+
 //format gallery markup
 createHTML = (data) => {
+    let gallery =document.getElementById('gallery')
     for(let i = 0; i < data.length; i++){
-        console.log(data[i])
         //HTML creation
-        let gallery =document.getElementById('gallery')
         let card = document.createElement('div');
         card.setAttribute('class', 'card')
         card.innerHTML = `
@@ -28,7 +29,46 @@ createHTML = (data) => {
         `
         gallery.appendChild(card)
     }
+    return  data
+}
 
+addEvent = (data) =>{
+    let cards = document.querySelectorAll('.card')
+    for(let i = 0; i < cards.length; i++){
+        cards[i].addEventListener('click', (e) => {
+            cards[i].insertAdjacentHTML('beforeend', `
+            <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="${data[i].picture.large}" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${data[i].name.first}</h3>
+                        <p class="modal-text">${data[i].email}</p>
+                        <p class="modal-text cap">${data[i].location.city}</p>
+                        <hr>
+                        <p class="modal-text">${data[i].phone}</p>
+                        <p class="modal-text">${data[i].location.street.number} ${data[i].location.street.name}, ${data[i].location.city}, ${data[i].location.postcode}</p>
+                        <p class="modal-text">Birthday: ${data[i].dob.date.slice(0, 10)}</p>
+                    </div>
+                </div>
 
+                <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                </div>
+            </div>`
+            )
+            //log the clicked card
+            let clicked = cards[i]
+            let clickedName = clicked.children[1].children[0].textContent
+            
+            console.log(data[i])
+            data[i]
+            console.log()
+            console.log(clickedName)
+
+        })
+    }
     
+
 }
