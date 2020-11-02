6794be1328a3c752e91a50df20924dca59807bb4
Lanfear7 <brennan1086@gmail.com>
2020-11-01 21:15:33 -0800

    search function works API will only return english names

--- js/script.js
+++ js/script.js
@@ -6,12 +6,24 @@ fetchData = (url) => {
         .then(data => createHTML(data)) //will creat new card elements that go to the DOM
         .then(data => addEvent(data)) //will add the event listener to each card
 }
-fetchData('https://randomuser.me/api/?results=12')
+fetchData('https://randomuser.me/api/?results=12&nat=us')
 
 
 //format gallery markup
 createHTML = (data) => {
     let gallery =document.getElementById('gallery')
+    let search = document.querySelector('.search-container')
+    search.innerHTML= `                       
+    <form action="#" method="get">
+        <input type="search" id="search-input" class="search-input" placeholder="Search...">
+        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form>`
+    search.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+            let value = e.target.value
+            searchPeople(value, data)
+        }
+    })    
     for(let i = 0; i < data.length; i++){
         //HTML creation
         let card = document.createElement('div');
@@ -25,7 +37,6 @@ createHTML = (data) => {
             <p class="card-text">${data[i].email}</p>
             <p class="card-text cap">${data[i].location.city}, ${data[i].location.street.name}</p>
         </div>
-        </div>
         `
         gallery.appendChild(card)
     }
@@ -34,33 +45,23 @@ createHTML = (data) => {
 
 formatPhoneNumber = (phoneNumberString) => {
     var cleaned = phoneNumberString.replace(/\D/g, '')
-    if (cleaned.length == 10) {
-        //reformat and return phone number
-        return cleaned.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
-    }
-
-    return null;
-  }
-
-addEvent = (data) =>{
-    let body = document.querySelector('body')
-    let cards = document.querySelectorAll('.card')
+    return cleaned.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+}
+popUp = (data,index) => {
     let gallery = document.querySelector('.gallery')
-    for(let i = 0; i < cards.length; i++){
-        cards[i].addEventListener('click', (e) => {
-            gallery.insertAdjacentHTML('afterend', `
+    gallery.insertAdjacentHTML('afterend', `
             <div class="modal-container">
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                     <div class="modal-info-container">
-                        <img class="modal-img" src="${data[i].picture.large}" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">${data[i].name.first}</h3>
-                        <p class="modal-text">${data[i].email}</p>
-                        <p class="modal-text cap">${data[i].location.city}</p>
+                        <img class="modal-img" src="${data[index].picture.large}" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${data[index].name.first}</h3>
+                        <p class="modal-text">${data[index].email}</p>
+                        <p class="modal-text cap">${data[index].location.city}</p>
                         <hr>
-                        <p class="modal-text">${formatPhoneNumber(data[i].phone)}</p>
-                        <p class="modal-text">${data[i].location.street.number} ${data[i].location.street.name}, ${data[i].location.city}, ${data[i].location.postcode}</p>
-                        <p class="modal-text">Birthday: ${data[i].dob.date.slice(0, 10)}</p>
+                        <p class="modal-text">${formatPhoneNumber(data[index].phone)}</p>
+                        <p class="modal-text">${data[index].location.street.number} ${data[index].location.street.name}, ${data[index].location.city}, ${data[index].location.postcode}</p>
+                        <p class="modal-text">Birthday: ${data[index].dob.date.slice(0, 10)}</p>
                     </div>
                 </div>
 
@@ -70,25 +71,52 @@ addEvent = (data) =>{
                 </div>
             </div>`
             )
-            //log the clicked card
-            let clicked = cards[i]
-            console.log(data[i])
+}
+addEvent = (data) =>{
+    let body = document.querySelector('body')
+    let cards = document.querySelectorAll('.card')
+    for(let i = 0; i < cards.length; i++){
+        cards[i].addEventListener('click', () => {
+            popUp(data,i)
+
+            //close pop up form
             let closeBtn = document.getElementById('modal-close-btn')
             closeBtn.addEventListener('click', () =>{
                 let popUp = document.querySelector('.modal-container')
                 body.removeChild(popUp)
                 
             })
-            
-            //card click
-            //clicked
-
-            //data on clicked card
-            //data[i]
- 
+            let prev = document.querySelector('#modal-prev')
+            let next = document.querySelector('#modal-next')
+            prev.addEventListener('click', () => {
+                let previous = --i
+                popUp(data, previous)
+            })
 
         })
     }
     
-
 }
+
+searchPeople = (value, data) => {
+    console.log('enter')
+    let name = value.toUpperCase()
+    let body = document.querySelector('body')
+    let gallery = document.querySelector('.gallery')
+    console.log('input is', name)
+    console.log(data)
+    for(let i = 0; i < data.length; i ++){
+        let userNames = data[i].name.first.toUpperCase()
+        console.log(userNames)
+        if(name == userNames){ 
+            popUp(data, i)
+            let closeBtn = document.getElementById('modal-close-btn')
+            closeBtn.addEventListener('click', () =>{
+                let popUp = document.querySelector('.modal-container')
+                body.removeChild(popUp)
+                
+            })
+        }
+    }
+}
+
